352b5e41dd9ab4df8966c528b6b195b79ec7f6b1
Jason Reeder <info@jasonreeder.com>
2017-11-05 11:18:08 -0800

    Fix breadcrumb

--- frontend/src/components/helpers.js
+++ frontend/src/components/helpers.js
@@ -5,12 +5,10 @@ export const Breadcrumb = ({ links, paths }) =>
   <nav className="breadcrumb" aria-label="breadcrumbs">
     <ul>
       {paths.map((p, i) =>
-        <li key={i}>
-          {i === paths.length - 1
-            ? p
-            : <Link to={links[p]}>
-                {p}
-              </Link>}
+        <li className={i === paths.length - 1 ? 'is-active' : ''} key={i}>
+          <Link to={links[p] || ''}>
+            {p}
+          </Link>
         </li>,
       )}
     </ul>
