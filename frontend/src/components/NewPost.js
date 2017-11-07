f5f0ffe2647ca5a2106f0e6067fe7073945a798c
Jason Reeder <info@jasonreeder.com>
2017-11-06 19:06:00 -0800

    Add placeholders

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -7,14 +7,42 @@ class NewPost extends Component {
         <div className="modal-background"> </div>
         <div class="modal-card">
           <header class="modal-card-head">
-            <p class="modal-card-title">Modal title</p>
+            <p class="modal-card-title">New post</p>
             <button class="delete" aria-label="close" />
           </header>
           <section class="modal-card-body">
-            <p>Content</p>
+            <div className="field">
+              <label className="label">Username</label>
+              <div className="control">
+                <input className="input" type="text" placeHolder="username" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Title</label>
+              <div className="control">
+                <input className="input" type="text" placeHolder="title" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Body</label>
+              <div className="control">
+                <input className="input" type="text" placeHolder="body" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Category</label>
+              <div className="control">
+                <div className="select">
+                  <select>
+                    <option>Category</option>
+                    <option>Category</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </section>
           <footer class="modal-card-foot">
-            <button class="button is-success">Save changes</button>
+            <button class="button is-success">Submit</button>
             <button class="button">Cancel</button>
           </footer>
         </div>
